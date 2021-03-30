00ce0073fcfaf7e3c1658cd1da3db93438da3c34
William-Moreno <intrcptr@comcast.net>
2021-03-30 16:56:58 -0700

    useAjax hook created, incorporated and functional for all CRUD

--- src/components/todo/Todo-connected.js
+++ src/components/todo/Todo-connected.js
@@ -1,83 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+import useAxios from '../hooks/ajax/useAjax.js';
 import TodoForm from './Form.js';
 import TodoList from './List.js';
 
 import './todo.scss';
 
-const todoAPI = 'https://api-js401.herokuapp.com/api/v1/todo';
-
 
 const ToDo = () => {
 
-  const [list, setList] = useState([]);
+  let [list, addItem, toggleComplete, removeItem, getTodoItems] = useAxios()
 
-  const _addItem = (item) => {
-    item.due = new Date();
-    item.complete = false;
-    axios.post(todoAPI, item)
-      .then(savedItem => {
-        setList([...list, savedItem.data]);
-      })
-      .catch(console.error);
-  };
 
-  const _toggleComplete = id => {
-
-    let item = list.filter(i => i._id === id)[0] || {};
-
-    if (item._id) {
-
-      item.complete = !item.complete;
-
-      let url = `${todoAPI}/${id}`;
-
-      axios.put(url, item)
-        .then(savedItem => {
-          setList(list.map(listItem => listItem._id === item._id ? savedItem.data : listItem));
-        })
-        .catch(console.error);
-    }
-  };
-
-  const _removeItem = id => {
-
-    console.log(id);
-
-    let item = list.filter(i => i._id === id)[0] || {};
-
-    if (item._id) {
-
-      let url = `${todoAPI}/${id}`;
-
-      axios.delete(url, item)
-        .then(removedItem => {
-
-    let temp = [...list];
-
-    for( let i = temp.length - 1 ; i >= 0 ; i--) {
-      if(removedItem.data._id === temp[i]._id) {
-        temp.splice(i, 1);
-        if(!temp.length) {
-          setList([]);
-          return;
-        }
-      }
-    }
-    setList(temp);
-
-  })
-        .catch(console.error);
-    }
-  };
-
-  const _getTodoItems = () => {
-    axios.get(todoAPI)
-      .then(result => setList(result.data.results))
-      .catch(console.error);
-  };
-
-  useEffect(_getTodoItems, []);
+  useEffect(getTodoItems, []);
 
     useEffect(() => {
     document.title = `To Do List: ${list.filter(item => !item.complete).length}`;
@@ -94,14 +28,14 @@ const ToDo = () => {
       <section className="todo">
 
         <div>
-          <TodoForm handleSubmit={_addItem} />
+          <TodoForm handleSubmit={addItem} />
         </div>
 
         <div>
           <TodoList
             list={list}
-            handleComplete={_toggleComplete}
-            handleDelete={_removeItem}
+            handleComplete={toggleComplete}
+            handleDelete={removeItem}
           />
         </div>
       </section>
